73a4e4bf8bb8b00f28a3526362b8405406327199
Cristobal <quilimacox1@gmail.com>
2024-09-04 07:51:40 -0500

    Se rraliza hook Usefetch

---
+++ src/hooks/UseFetch.jsx
@@ -0,0 +1,27 @@
+import axios from "axios"
+import { useState } from "react"
+
+
+const useFetch = url => {
+
+    const[state, setState] = useState()
+    const[hasError, setHasError] = useState(false)
+
+
+const getData = () =>{
+    axios.get(url)
+    .then(res => {setState(res.data)
+    setHasError
+    })
+
+    .catch(err => {console.log(err)
+    setHasError(true)
+    })
+    
+}
+
+return[state, getData, hasError]
+
+}
+
+export default useFetch
