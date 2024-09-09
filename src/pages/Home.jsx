13fad533660fb47cbdec605dae2b5a08bb52f2a5
Cristobal <quilimacox1@gmail.com>
2024-09-09 11:19:45 -0500

    Se ajusta home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+
+import { useEffect } from "react"
 import useFetch from "../hooks/UseFetch"
 
 const Home = ({url}) => {
+
+const [ pokemon, getPokemonById,  ] = useFetch(url)
+
+useEffect(() => {
+getPokemonById()
+
+    }, [])
+
   return (
     <div>Home</div>
   )
