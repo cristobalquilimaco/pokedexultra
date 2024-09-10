0ecf3b3f421aaa657183cfa2192a4f60008a90ce
Cristobal <quilimacox1@gmail.com>
2024-09-10 11:02:25 -0500

    Se agreag useffect

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react"
 import usefecth from "../hooks/UseFetch"
 
 const Pokedex = () => {
     const urlBase = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
     const [pokemons, getAllPokemos] = usefecth(urlBase)
-  return (
+    
+  useEffect(() => {
+    getAllPokemons(urlBase)
+
+  }, [urlBase]);
+  
+  
+  
+    return (
     <div>Pokedex</div>
   )
 }
