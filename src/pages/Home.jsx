2d7f9819f12f50efdc46f7cb39b850327e90c0ac
Cristobal <quilimacox1@gmail.com>
2024-09-12 19:14:32 -0500

    Se inicia pantalla home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@
 const Home = () => {
   return (
     <div>
+      <h1>POKEDEX</h1>
+      <h2>Hi Trainer</h2>
+      <h3>Pres Star</h3>
+      <form>
+        <input type="text" />
+        <button>Catch them all!</button>
+        </form>  
     </div>
   )
 }
