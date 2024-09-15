d5ba474ab34380df63679915eba7f7b375e69615
Cristobal <quilimacox1@gmail.com>
2024-09-14 20:39:50 -0500

    Se agreaga boton submit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 
 const Home = () => {
+
+  const handleSubmit = e =>{
+    
+  }
+
   return (
     <div>
       <h1>POKEDEX</h1>
       <h2>Hi Trainer</h2>
-      <h3>Pres Star</h3>
-      <form>
+      <h3>Press Star</h3>
+      <form onSubmit={handleSubmit}>
         <input type="text" />
         <button>Catch them all!</button>
         </form>  
