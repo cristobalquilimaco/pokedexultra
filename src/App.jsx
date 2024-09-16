12e5c7f3cbcc7070f1eae52e459ab2c9ea117da1
Cristobal <quilimacox1@gmail.com>
2024-09-16 18:21:07 -0500

    Se agrega protectedRoutes a routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Pokedex from "./pages/Pokedex"
+import ProtectedRoutes from "./pages/ProtectedRoutes"
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
-          <Route>
+          <Route element={<ProtectedRoutes/>}>
             <Route path="/pokedex" element={<Pokedex/>}/>
           </Route>
       </Routes>
