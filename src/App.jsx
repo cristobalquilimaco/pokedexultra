95218bcc64fcf72668986568dd93feb439f2a6b1
Cristobal <quilimacox1@gmail.com>
2024-09-16 18:17:24 -0500

    Se importa Pokedex a app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Pokedex from "./pages/Pokedex"
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/pokedex" element={<Home/>}/>
+        <Route path="/pokedex" element={<Pokedex/>}/>
       </Routes>
     </div>
   )
