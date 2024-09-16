29aeb04e9bd5ae9cb4bd288af0624b6d56ae70fb
Cristobal <quilimacox1@gmail.com>
2024-09-16 18:16:52 -0500

    Se agrega pokedex

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,24 +1,8 @@
-// import { useEffect } from "react"
-// import usefecth from "../hooks/UseFetch"
-// import Pokecard from "../components/Pokecard"
 
-// const Pokedex = () => {
-//     const urlBase = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-//     const [pokemon, getAllPokemons] = usefecth(urlBase)
-    
-//   useEffect(() => {
-//     getAllPokemons(urlBase)
+const Pokedex = () => {
+  return (
+    <div>Pokedex</div>
+  )
+}
 
-//   }, [urlBase]);
-  
-  
-//     return (
-// <div>
-//   <Pokecard
-//     urlBase={urlBase}
-//   />
-// </div>
-//   )
-// }
-
-// export default Pokedex
+export default Pokedex
