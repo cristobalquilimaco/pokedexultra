b89c7d53ab9c1d128e45d28f6d12214d2b6bf621
Cristobal <quilimacox1@gmail.com>
2024-09-16 18:26:44 -0500

    Se agrega logica a protectedRoutes

--- src/pages/ProtectedRoutes.jsx
+++ src/pages/ProtectedRoutes.jsx
@@ -1,8 +1,14 @@
+import { useSelector } from "react-redux"
+import { Outlet } from "react-router-dom"
 
 const ProtectedRoutes = () => {
-  return (
-    <div>ProtectedRoutes</div>
-  )
-}
+    const {trainerName} = useSelector(states => states)
+
+    if(trainerName.length >= 3){
+        return <Outlet />
+    }else{
+
+        }
+    }
 
 export default ProtectedRoutes
