9f7c3b8ebf4a5fa5a7785b32bd5ec645db453999
Cristobal <quilimacox1@gmail.com>
2024-09-16 18:57:40 -0500

    Se importa useEffect

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,5 +1,16 @@
+import { useEffect } from "react"
+import useFetch from "../hooks/UseFetch"
 
 const Pokedex = () => {
+
+    const url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
+    const [pokemons, getAllPokemons] = useFetch(url)
+
+    useEffect(() => {
+    getAllPokemons()
+    }, [])
+    
+    console.log(pokemons)
   return (
     <div>Pokedex</div>
   )
