52fd3b01dbc17a8de30d38a233e2213a2cd678fe
Cristobal <quilimacox1@gmail.com>
2024-09-16 19:01:17 -0500

    Se agrega trainerName

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,7 +1,10 @@
 import { useEffect } from "react"
 import useFetch from "../hooks/UseFetch"
+import { useSelector } from "react-redux"
 
 const Pokedex = () => {
+
+    const trainerName = useSelector(state => state.trainerName )
 
     const url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0"
     const [pokemons, getAllPokemons] = useFetch(url)
@@ -12,7 +15,9 @@ const Pokedex = () => {
     
     console.log(pokemons)
   return (
-    <div>Pokedex</div>
+    <div>Pokedex
+        <h1>Bienvenido {trainerName} ! Busca tu pokemon</h1>
+    </div>
   )
 }
 
