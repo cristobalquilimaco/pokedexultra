d48adc64a25c42bfdf4c2073b912dd806f997c0a
Cristobal <quilimacox1@gmail.com>
2024-09-23 19:32:14 -0500

    Se importa pokedex.css

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import useFetch from "../hooks/UseFetch";
 import { useSelector } from "react-redux";
 import PokeContainer from "../components/Pokedex/PokeContainer";
+import "./styles/pokedex.css"
 
 const Pokedex = () => {
   const trainerName = useSelector(states => states.trainerName);
@@ -16,7 +17,10 @@ const Pokedex = () => {
     <div>
       <h1>Bienvenido {trainerName}! Busca tu Pokémon</h1>
       {/* Asegúrate de que pokemons sea un objeto con results como un array */}
+      <section className="poke_container_class">
       <PokeContainer pokemons={pokemons?.results || []} />
+      </section>
+      
     </div>
   );
 };
