f2dde074d5cb2f86ac8731bc7517295a4a014881
Cristobal <quilimacox1@gmail.com>
2024-09-23 19:25:18 -0500

    Se modifica cantidad de pokemons en la url

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -5,7 +5,7 @@ import PokeContainer from "../components/Pokedex/PokeContainer";
 
 const Pokedex = () => {
   const trainerName = useSelector(states => states.trainerName);
-  const url = "https://pokeapi.co/api/v2/pokemon?limit=20&offset=0";
+  const url = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
   const [pokemons, getAllPokemons] = useFetch(url);
 
   useEffect(() => {
