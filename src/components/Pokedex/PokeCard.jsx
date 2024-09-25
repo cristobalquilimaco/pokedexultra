ce95846b8121e2a860c8cf8f674bd6b1e5eef41d
Cristobal <quilimacox1@gmail.com>
2024-09-24 21:17:38 -0500

    Se ajusta pokecard

--- src/components/Pokedex/PokeCard.jsx
+++ src/components/Pokedex/PokeCard.jsx
@@ -15,40 +15,38 @@ const Pokecard = ({ url }) => {
     const darkBackgroundClass = pokemon?.types[0]?.type.name ? `type-${pokemon.types[0].type.name}-dark` : '';
 
     return (
-        <div className={`pokecard ${backgroundClass}`}> {/* Aplica el fondo aquí */}
-            <header className="poke_header">
-                <img className="poke_card_img" src={pokemon?.sprites?.other.home.front_default} alt={pokemon?.name} />
-            </header>
-            <section>
-                <article className={`pokecard_name ${darkBackgroundClass}`}>
-                <h3 className="name_text" >{pokemon?.name}</h3>
-                </article>
-                
-                <ul>
-                    {
-                        pokemon?.types.map(typeInfo => (
-                            <li
-                                key={typeInfo.type.url}
-                            >
-                                {typeInfo.type.name}
-                            </li>
-                        ))
-                    }
-                </ul>
-            </section>
-            <footer>
-                <ul>
-                    {
-                        pokemon?.stats.map(statsInfo => (
-                            <li key={statsInfo.stat.url}>
-                                <span>{statsInfo.stat.name}</span>
-                                <span>{statsInfo.base_stat}</span>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </footer>
-        </div>
+<div className="card">
+  <div className="bg"></div>
+  <div className="blob"></div>
+  <div className={`pokecard ${backgroundClass}`}> {/* Aplica el fondo aquí */}
+    <header className="poke_header">
+      <img className="poke_card_img" src={pokemon?.sprites?.other.home.front_default} alt={pokemon?.name} />
+    </header>
+    <section>
+      <article className={`pokecard_name ${darkBackgroundClass}`}>
+        <h3 className="name_text">{pokemon?.name}</h3>
+      </article>
+      <ul>
+        {pokemon?.types.map(typeInfo => (
+          <li key={typeInfo.type.url}>
+            {typeInfo.type.name}
+          </li>
+        ))}
+      </ul>
+    </section>
+    <footer>
+      <ul>
+        {pokemon?.stats.map(statsInfo => (
+          <li key={statsInfo.stat.url}>
+            <span>{statsInfo.stat.name}</span>
+            <span>{statsInfo.base_stat}</span>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  </div>
+</div>
+
     );
 };
 
