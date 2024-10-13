f1aee52b8b34b5d6e7e48e3c5513d080ca5da0d0
Cristobal <quilimacox1@gmail.com>
2024-10-12 19:59:38 -0500

    Se ajusta pokedex en Pokecard

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -1,26 +1,28 @@
-import { useParams } from "react-router-dom"
-import useFetch from "../hooks/UseFetch"
-import { useEffect } from "react"
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/UseFetch";
+import { useEffect } from "react";
 
 const PokedexName = () => {
-
-  const { name } = useParams()
-
-  const url = `https://pokeapi.co/api/v2/pokemon/${name}`
+  const { name } = useParams();
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`;
   
-  const [pokemon, getPokemonByName] = useFetch(url)
+  const [pokemon, getPokemonByName] = useFetch(url);
 
   useEffect(() => {
-    getPokemonByName()
-  }, [name])
-  
+    getPokemonByName();
+  }, [name, getPokemonByName]);
+
+  // Maneja el caso en que pokemon es undefined
+  if (!pokemon) {
+    return <p>Cargando...</p>;
+  }
 
   return (
     <div>
-      <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+      <img src={pokemon.sprites.other["official-artwork"].front_default} alt={pokemon.name} />
       <h1>{pokemon.name}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default PokedexName
+export default PokedexName;
