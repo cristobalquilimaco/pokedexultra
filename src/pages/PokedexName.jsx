ab35aa7db94c8aee7c2e115fbb32615087afe526
Cristobal <quilimacox1@gmail.com>
2024-10-12 19:24:41 -0500

    Se agrega nombre de pokemon

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -1,16 +1,25 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../hooks/UseFetch"
+import { useEffect } from "react"
 
 const PokedexName = () => {
 
   const { name } = useParams()
 
-  const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+  const url = `https://pokeapi.co/api/v2/pokemon/${name}`
   
   const [pokemon, getPokemonByName] = useFetch(url)
 
+  useEffect(() => {
+    getPokemonByName()
+  }, [name])
+  
+
   return (
-    <div>{name}</div>
+    <div>
+      <img src={pokemon?.sprites.other["official-artwork"].front_default} alt="" />
+      <h1>{pokemon.name}</h1>
+    </div>
   )
 }
 
