aa572c0d305c2be7c0d0c952565352c48fb86b4f
Cristobal <quilimacox1@gmail.com>
2024-10-12 19:24:01 -0500

    Se agrega imagen de pokemon en pokedexName

--- src/pages/PokedexName.jsx
+++ src/pages/PokedexName.jsx
@@ -1,8 +1,13 @@
 import { useParams } from "react-router-dom"
+import useFetch from "../hooks/UseFetch"
 
 const PokedexName = () => {
 
   const { name } = useParams()
+
+  const url = "https://pokeapi.co/api/v2/pokemon/ditto"
+  
+  const [pokemon, getPokemonByName] = useFetch(url)
 
   return (
     <div>{name}</div>
