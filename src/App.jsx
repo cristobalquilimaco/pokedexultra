29b5add61b75f9618cde975d61ddc427b4e488c4
Cristobal <quilimacox1@gmail.com>
2024-10-17 22:36:29 -0500

    Se agrega logo

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Pokedex from "./pages/Pokedex"
 import ProtectedRoutes from "./pages/ProtectedRoutes"
 import "../src/app.css"
 import PokedexName from "./pages/PokedexName"
+import Nabvar from "./shared/Navbar/Nabvar"
 
 const App = () => {
   return (
@@ -11,8 +12,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
           <Route element={<ProtectedRoutes/>}>
-            <Route path="/pokedex" element={<Pokedex/>}/>
-            <Route path="/pokedex/:name" element={<PokedexName/>}/>
+          <Route path="/pokedex" element={<><Nabvar/><Pokedex /></>} />
+            <Route path="/pokedex/:name" element={<><Nabvar/><PokedexName /></>}/>
           </Route>
       </Routes>
     </div>
--- src/shared/Navbar/Nabvar.jsx
+++ src/shared/Navbar/Nabvar.jsx
@@ -1,6 +1,10 @@
+import logo from "../../assets/images/logo.png"
+
 const Nabvar = () => {
   return (
-    <div>Nabvar</div>
+    <div>
+        <img src={logo} alt="" />
+    </div>
   )
 }
 
