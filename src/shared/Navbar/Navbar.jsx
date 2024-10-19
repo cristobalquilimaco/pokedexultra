e99f3440bef6482fd9877509fa80a072fb361c8c
Cristobal <quilimacox1@gmail.com>
2024-10-18 20:27:12 -0500

    Se ajusta header de pokedex

--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom"
 import logo from "../../assets/images/logo.png"
 import "./styles/navbar.css"
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <h1 className="logo">POKEDEX</h1>
         <img className="img__logo" src={logo} alt="" />
+        <Link to={"/pokedex"}><h1 className="logo">POKEDEX</h1></Link>
     </nav>
   )
 }
