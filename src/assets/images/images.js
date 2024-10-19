9d57e91cc79153cf11223f35954054b146dfb33d
Cristobal <quilimacox1@gmail.com>
2024-10-18 21:05:39 -0500

    Se ajusta header

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,8 +1,15 @@
 import avatarAsh from "../../assets/images/ash.png"
+import avatarBrock from "../../assets/images/brock.png"
+import avatarJames from "../../assets/images/james.png"
+import avatarJessie from "../../assets/images/jessie.png"
+import avatarMisty from "../../assets/images/misty.png"
 
 const avatarImg = {
     avatarAsh,
-
+    avatarBrock,
+    avatarJames,
+    avatarJessie,
+    avatarMisty
 }
 
 export default avatarImg
--- src/shared/Navbar/Header.jsx
+++ src/shared/Navbar/Header.jsx
@@ -5,8 +5,10 @@ import "./styles/header.css"
 const Header = () => {
   return (
     <header className="navbar">
-        <img className="img__logo" src={logo} alt="" />
-        <Link to={"/pokedex"}><h1 className="logo">POKEDEX</h1></Link>
+      <div className="logo__section">
+      <img className="img__logo" src={logo} alt="" />
+      <Link to={"/pokedex"}><h1 className="logo">POKEDEX</h1></Link>
+      </div>
     </header>
   )
 }
