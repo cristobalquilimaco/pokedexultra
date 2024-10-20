ff0d14f4ec1efdb16c639c0b9baddb45fcdd5079
Cristobal <quilimacox1@gmail.com>
2024-10-20 18:59:21 -0500

    Se ajusta paginacion

--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -60,9 +60,7 @@ const Pokedex = () => {
 
   return (
     <div className="search__pokemon">
-
       <h1>Bienvenido! Busca tu Pokémon</h1>
-  
       <form onSubmit={handleSubmit}>
         <input ref={searchPokemon} type="text" />
         <button type="submit">Search</button>
