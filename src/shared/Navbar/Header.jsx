65c66a115c432b5d1dd8f7295cd51df5d750d810
Cristobal <quilimacox1@gmail.com>
2024-10-20 19:54:52 -0500

    Se ajusta header

--- src/shared/Navbar/Header.jsx
+++ src/shared/Navbar/Header.jsx
@@ -9,16 +9,17 @@ const Header = () => {
 const {name, avatar} = useSelector(states => states.trainerName);
 
   return (
-    <header className="navbar">
-      <div className="logo__section">
-      <img className="img__logo" src={logo} alt="" />
-      <Link to={"/pokedex"}><h1 className="logo">POKEDEX</h1></Link>
-      </div>
-      <div className="user__info">
+<header className="navbar">
+
+    <Link className="logo__section" to={"/pokedex"}>
+        <img className="img__logo" src={logo} alt="" />
+        <h1 className="logo">POKEDEX</h1></Link>
+
+    <div className="user__info">
         <p className="name__trainer">{name}</p>
         <img className="avatar" src={avatar} alt="" />
-      </div>
-    </header>
+    </div>
+</header>
   )
 }
 
